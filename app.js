fdffd7cd1696040ae46eca4002f1c32fa740fbba
Clay James <clayw.james@gmail.com>
2016-06-26 17:21:45 -0400

    Add home location support and DOM object

--- app.js
+++ app.js
@@ -4,6 +4,44 @@ const request = require('request')
 const fs = require('fs')
 
 
+const DOM = {
+  date: document.getElementById('date').value,
+  locationsDiv: document.getElementById('locations'),
+
+  getInputElements(){
+    let childArray = Array.prototype.slice.call(this.locationsDiv.childNodes) //converts childNodes object list into an array
+    let inputElements = childArray.filter((element) => {
+      if(element.tagName === 'INPUT') return true;
+      return false;
+    })
+    return inputElements;
+  },
+  setHomeInputs(){
+    var inputNodes = this.locationsDiv.childNodes;
+    for(var child = inputNodes[0]; child !== null; child = child.nextElementSibling){
+      if(child.className == 'home'){
+        child.value = 'HOME';
+      }
+    }
+  },
+  deleteHomeInputs(){
+    var inputNodes = this.locationsDiv.childNodes;
+    for(var child = inputNodes[0]; child !== null; child = child.nextElementSibling){
+      if(child.className == 'home'){
+        this.locationsDiv.removeChild(child);
+        child = inputNodes[0];
+      }
+    }
+  },
+  createNewLocationInput(activeElement){
+    activeElement.insertAdjacentHTML('afterend', '<br><input>')
+  },
+  createNewAddressInput(activeElement){
+    activeElement.insertAdjacentHTML('afterend', '<label>Address: </label><input class="address">')
+  }
+}
+
+
 const mapquest = {
   //The object that interacts with the mapquest API
   baseURL: 'http://www.mapquestapi.com/directions/v2/route?key=MkRTKx7DbBySjsya4hnVsQ0bxgQgnbSy',
@@ -46,25 +84,36 @@ const savedAddresses = {
 }
 
 
-const locationsDiv = document.getElementById('locations')
+//initialize
+setTimeout(function(){
+  //create saved addresses file if it doesn't exist
+  fs.stat('addresses.txt', (err) => {
+    if(err && err.code == 'ENOENT') fs.closeSync(fs.openSync('addresses.txt', 'w'))
+  })
+  if(savedAddresses.isLocation('HOME')){
+    DOM.setHomeInputs();
+  }else{
+    alert('You have no set home location.  Setting a home location will reduce '+
+    'the work you need to do.  To learn about home locations, please read the instructions, '+
+    'which you can access from the start page.  To set a home location, visit the settings page '+
+    'which is also accesable from the start page.');
+    DOM.deleteHomeInputs();
+  }
+}, 100)
+
+
 document.addEventListener('keydown', event => {
   if(event.keyCode === 9){ //tab key
-    var locationElements = Array.prototype.slice.call(locationsDiv.childNodes) //converts childNodes object list into an array
-    if(locationElements.includes(document.activeElement)){ //if a location input has focus
+    var inputElements = DOM.getInputElements();
+    //var locationElements = Array.prototype.slice.call(DOM.locationsDiv.childNodes) //converts childNodes object list into an array
+    if(inputElements.includes(document.activeElement)){ //if a location input has focus
       if(document.activeElement.class === 'address'){
-        locationsDiv.appendChild(document.createElement('br'))
-        locationsDiv.appendChild(document.createElement('input'))
+        DOM.createNewLocationInput(document.activeElement);
       }else{
         if(savedAddresses.isLocation(document.activeElement.value)){
-          locationsDiv.appendChild(document.createElement('br'))
-          locationsDiv.appendChild(document.createElement('input'))
+          if(document.activeElement.className !== 'home') DOM.createNewLocationInput(document.activeElement);
         }else{
-          var label = document.createElement('label')
-          label.innerHTML = 'Address: ';
-          var addressInput = document.createElement('input')
-          addressInput.class = 'address';
-          locationsDiv.appendChild(label)
-          locationsDiv.appendChild(addressInput)
+          DOM.createNewAddressInput(document.activeElement);
         }
       }
     }
@@ -73,23 +122,26 @@ document.addEventListener('keydown', event => {
 
 const submitButton = document.getElementById('submit')
 submitButton.onclick = function(){
-  var date = document.getElementById('date').value;
-  var inputElements = Array.prototype.slice.call(locationsDiv.childNodes) //converts childNodes object list into an array
-  var inputs = inputElements.filter((element) => {
-    if(element.tagName === 'INPUT') return true;
-    return false;
-  })
+  var date = DOM.date;
+  var inputElements = DOM.getInputElements()
   var locations = []
-  var j = 0;
-  for(let i = 0; i < inputs.length; i++){
-    if(inputs[i].class === 'address'){
-      let address = inputs[i].value;
-      let location = inputs[i - 1].value;
+  for(let i = 0; i < inputElements.length; i++){
+    if(inputElements[i].className === 'address'){
+      let address = inputElements[i].value;
+      let location = inputElements[i - 1].value;
       savedAddresses.saveAddress(location, address)
-    }else{
-      locations[j] = inputs[i].value
-      //check if location exists
-      j++
+    }
+  }
+  var j = 0;
+  for(let i = 0; i < inputElements.length; i++){
+    if(inputElements[i].className == "" || inputElements[i].className == 'home'){
+      if(savedAddresses.isLocation(inputElements[i].value)){
+        locations[j] = inputElements[i].value
+        j++
+      }else{
+        alert('Please enter the address for ' + inputElements[i].value)
+        return false
+      }
     }
   }
   var addresses = [];
